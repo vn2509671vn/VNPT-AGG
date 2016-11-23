9a5af4b7f2f16f7916e055c3868254e0f47642fa
Hiển AGG <minhhien20091993@gmail.com>
2016-11-23 15:14:21 +0700

    Commit source

---
+++ VNPT_BCS/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace VNPT
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+
+
+        protected void ASPxButton1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("WebForm1.aspx");
+        }
+}      
+}
---
+++ VNPT_BCS/WebForm1.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="VNPT_BCS.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+    Hiển</p>
+</asp:Content>
+

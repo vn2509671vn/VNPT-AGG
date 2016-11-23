31343fdceb993cb0791c58acbb2846dd53a2f2e4
Hiển AGG <minhhien20091993@gmail.com>
2016-11-23 16:01:34 +0700

    Masterpage + image

---
+++ VNPT-BSC/VNPT-BSC/VNPT-BSC/MasterPage.master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace VNPT
+{
+    public partial class MasterPage : System.Web.UI.MasterPage
+    {
+       
+        
+    }
+}

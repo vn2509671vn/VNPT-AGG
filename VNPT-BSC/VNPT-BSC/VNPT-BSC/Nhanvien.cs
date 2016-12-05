d15406ff4fd0d291b0ebbfa5ebbbcb3a5ac20c65
Hiển AGG <minhhien20091993@gmail.com>
2016-12-05 16:43:10 +0700

    commit code

---
+++ VNPT-BSC/VNPT-BSC/VNPT-BSC/Nhanvien.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VNPT_BSC
+{
+    public class Nhanvien
+    {
+     
+        public int nhanvien_id {get;set;}
+        public string nhanvien_taikhoan { get; set; }
+        public string nhanvien_matkhau { get; set; }
+        public string nhanvien_hoten { get; set; }
+        public string nhanvien_donvi { get; set; }
+        public string nhanvien_chucvu { get; set; }
+        
+    }
+}
---
+++ VNPT-BSC/VNPT-BSC/VNPT-BSC/NhanvienSession.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+
+namespace VNPT_BSC
+{
+    public static class NhanvienSession
+    {
+        
+
+        public static void SetCurrentUser(this HttpSessionState session, Nhanvien nhanvien)
+        {
+            session["currentUser"] = nhanvien;
+           
+        }
+        public static Nhanvien GetCurrentUser(this HttpSessionState session)
+        {
+            return session["currentUser"] as Nhanvien;
+            
+        }
+    }
+}

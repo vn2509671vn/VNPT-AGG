989413a50b7729b4088a5571621205384de00f30
ADMIN-PC\ADMIN <vn2509671vn@gmail.com>
2016-12-08 10:13:04 +0700

    Update master page

--- VNPT-BSC/VNPT-BSC/VNPT-BSC/MasterPage.master.cs
+++ VNPT-BSC/VNPT-BSC/VNPT-BSC/MasterPage.master.cs
@@ -11,13 +11,14 @@ namespace VNPT_BSC
 {
     public partial class MasterPageEdit : System.Web.UI.MasterPage
     {
-        
+        public static int chucvu;
         protected void Page_Load(object sender, EventArgs e)
         {
             Nhanvien nhanvien = Session.GetCurrentUser();
             lblUser.Text = "Chào: <b>" + nhanvien.nhanvien_hoten + "</b>";
             lblDonvi.Text = nhanvien.nhanvien_donvi;
             lblChucvu.Text = nhanvien.nhanvien_chucvu;
+            chucvu = nhanvien.nhanvien_chucvu_id;
         }
     }
 }
